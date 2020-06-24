7c944c7c1b88bb4d86f065385896afd7890802eb
Tawana Howard <tawanahow@gmail.com>
2020-06-24 12:27:04 -0400

    add geolocation api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  //https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
+  window.navigator.geolocation.getCurrentPosition(
+    //success call back function
+    //err call back function if postiion is not located
+    (position) => console.log(position),
+    (err) => console.log(err)
+  );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return <div>Hello</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
