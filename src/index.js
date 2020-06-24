230819f14d41360376b7f31c1d87e014e960ebfd
Tawana Howard <tawanahow@gmail.com>
2020-06-24 17:11:52 -0400

    refactor state

--- src/index.js
+++ src/index.js
@@ -2,25 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = { lat: null, errorMessage: '' }; //initialize state to null; we don't know that lat yet
+  //   //initialize state to null; we don't know that lat yet
+  //   this.state = { lat: null, errorMessage: '' };
+  // }
 
+  state = { lat: null, errorMessage: '' };
+
+  componentDidMount() {
     //https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
     window.navigator.geolocation.getCurrentPosition(
       //success call back function
       //err call back function if postiion is not located
-      (position) => {
-        //setState is called anytime state is to be update
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      //setState is called anytime state is to be update
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
+  componentDidUpdate() {
+    console.log('This component re-rendered');
+  }
+  //reminder render it to return JSX ONLY
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
