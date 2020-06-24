79261793c99aa5b9b7c6abe81d58d5f7efc5d476
Tawana Howard <tawanahow@gmail.com>
2020-06-24 13:26:02 -0400

    edit error handling

--- src/index.js
+++ src/index.js
@@ -2,16 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, errorMessage: '' }; //initialize state to null; we don't know that lat yet
+
     //https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
     window.navigator.geolocation.getCurrentPosition(
       //success call back function
       //err call back function if postiion is not located
-      (position) => console.log(position),
-      (err) => console.log(err)
+      (position) => {
+        //setState is called anytime state is to be update
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
+  }
 
-    return <div>Latitude: </div>;
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    return <div>Loading!</div>;
   }
 }
 
