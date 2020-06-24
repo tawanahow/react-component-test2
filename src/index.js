bc1997e0a2567463c0487e84cb40c6184c17095a
Tawana Howard <tawanahow@gmail.com>
2020-06-24 12:57:38 -0400

    revise app to class component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  //https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
-  window.navigator.geolocation.getCurrentPosition(
-    //success call back function
-    //err call back function if postiion is not located
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
+class App extends React.Component {
+  render() {
+    //https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
+    window.navigator.geolocation.getCurrentPosition(
+      //success call back function
+      //err call back function if postiion is not located
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
 
-  return <div>Hello</div>;
-};
+    return <div>Latitude: </div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
